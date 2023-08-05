00d2cc309303eebd88b3f0e05108235e5a9cf282
Minhajul Karim <project@zaagsys.com>
2023-08-05 11:03:20 +0600

    Implement page routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Main from './components/Main';
-import './main.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BlogDetails from "./pages/BlogDetails";
+import Layout from "./pages/Layout";
+import "./main.css";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Main />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/blogs/:blogId" element={<BlogDetails />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
